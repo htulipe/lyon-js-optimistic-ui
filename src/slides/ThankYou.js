7208b00ce0d3d7ec0f46575574ef7777502c44f4
Hadrien <hadrien.tulipe@webedia-group.com>
2019-11-25 18:00:12 +0100

    final touch

--- src/slides/ThankYou.js
+++ src/slides/ThankYou.js
@@ -5,6 +5,12 @@ export const ThankYou = () => {
   return (
     <TitleSlide>
       <h1>Merci !</h1>
+
+      <footer>
+        Slides faits avec{' '}
+        <a href="https://github.com/bvaughn/react-presents">react-presents</a>{' '}
+        de Brian Vaughn
+      </footer>
     </TitleSlide>
   );
 };
